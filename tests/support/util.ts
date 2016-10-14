6ae5bb00543289527156850d86011fcca93f99a6
Kitson Kelly <me@kitsonkelly.com>
2016-10-14 09:05:48 +0100

    Type assertions for core.d.ts

--- tests/support/util.ts
+++ tests/support/util.ts
@@ -1,12 +1,11 @@
 import { includes } from 'dojo-shim/array';
-import TypeWriter from './TypeWriter';
+import TypeWriter, { TypeWriterResult } from './TypeWriter';
 import {
 	createCompilerHost,
 	createProgram,
 	CompilerOptions,
 	ModuleKind,
-	ScriptTarget,
-	SyntaxKind
+	ScriptTarget
 } from 'typescript';
 
 /**
@@ -28,18 +27,13 @@ export function throwImmediatly() {
 	throw new Error('unexpected code path');
 }
 
-export interface TypesForFile {
-	syntaxKind: SyntaxKind;
-	sourceText: string;
-	type: string;
-}
-
 export interface TypesForFiles {
-	[filename: string]: TypesForFile[];
+	[filename: string]: TypeWriterResult[];
 }
 
 /**
  * A function which returns a map of arrays representing the types for each file
+ *
  * @param fileNames The file names to generate the types for
  */
 export function getTypeForFiles(...fileNames: string[]): TypesForFiles {
@@ -53,10 +47,7 @@ export function getTypeForFiles(...fileNames: string[]): TypesForFiles {
 	const results: TypesForFiles = {};
 	program.getSourceFiles().forEach((sourceFile) => {
 		if (includes(fileNames, sourceFile.fileName)) {
-			results[sourceFile.fileName] = typeWriter.getTypeAndSymbols(sourceFile.fileName)
-				.map(({ syntaxKind, sourceText, type }) => {
-					return { syntaxKind, sourceText, type };
-				});
+			results[sourceFile.fileName] = typeWriter.getTypeAndSymbols(sourceFile.fileName);
 		}
 	});
 	return results;
