43ed09c79da58f9f14b4cfebe1fed9cbd4abd504
Kitson Kelly <me@kitsonkelly.com>
2016-10-14 09:50:40 +0100

    Reconfigure grunt/CI for testing types only

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import * as abilities from './abilities';
+import * as bases from './bases';
+import * as core from './core';
+import * as observables from './observables';
+import * as widgetBases from './widgetBases';
 
 export = {
-	abilities
+	abilities,
+	bases,
+	core,
+	observables,
+	widgetBases
 };
