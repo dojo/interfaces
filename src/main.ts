c7f27b31765bc275db4b74f885d264208390948d
Bradley Maier <brdlmaier49@gmail.com>
2016-12-15 19:38:24 -0500

    Remove observable interface (#53)

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import * as abilities from './abilities';
 import * as bases from './bases';
 import * as core from './core';
 import * as loader from './loader';
-import * as observables from './observables';
 import * as shim from './shim';
 import * as vdom from './vdom';
 
@@ -11,7 +10,6 @@ export = {
 	bases,
 	core,
 	loader,
-	observables,
 	shim,
 	vdom
 };
--- tests/unit/all.ts
+++ tests/unit/all.ts
@@ -2,6 +2,5 @@ import './abilities';
 import './bases';
 import './core';
 import './loader';
-import './observables';
 import './shim';
 import './vdom';
