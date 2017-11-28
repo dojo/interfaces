d273732fa103101206cb8d5356f4d5d5390331a1
Bryan Forbes <bryan@reigndropsfall.net>
2017-11-28 16:07:06 -0600

    Remove obsolete interfaces

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,7 @@
-import * as abilities from './abilities';
-import * as bases from './bases';
 import * as cli from './cli';
-import * as core from './core';
 import * as loader from './loader';
 
 export = {
-	abilities,
-	bases,
 	cli,
-	core,
 	loader
 };
--- tests/unit/all.ts
+++ tests/unit/all.ts
@@ -1,5 +1,2 @@
-import './abilities';
-import './bases';
 import './cli';
-import './core';
 import './loader';
