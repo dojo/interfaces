8c89b242158fee6c1c6091f58b747cd22a2e6ae6
Bryan Forbes <bryan@reigndropsfall.net>
2017-11-28 15:22:07 -0600

    Remove shim dependency and add removals back to main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
+import * as abilities from './abilities';
+import * as bases from './bases';
 import * as cli from './cli';
+import * as core from './core';
 import * as loader from './loader';
 
 export = {
+	abilities,
+	bases,
 	cli,
+	core,
 	loader
 };
